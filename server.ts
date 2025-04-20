f399f405120cc6c4da934dc863b1423aa52de450
Maro7772 <mm9922522@gmail.com>
2025-04-20 20:43:59 +0200

    Third

--- server.ts
+++ server.ts
@@ -4,3 +4,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
