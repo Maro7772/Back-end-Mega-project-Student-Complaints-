059a5e37f3c4f262d08ab6d50ece8cea4f585487
Omar Yasser <anaesthesiayassermadi717@gmail.com>
2025-05-09 01:52:13 +0300

    auth-version-3

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import express from "express";
-import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
-=======
+import cookieParser from "cookie-parser";
 import "dotenv/config"; // Load environment variables from .env file
 import express, { Request, Response } from "express";
 import env from "./util/validateEnv";
->>>>>>> 0f56d3f9fd3c100be18820f24a4c0564a88d9d1d
 import { connectDB  } from "./config/db";
 import router from "./routes/auth.route";
 import ComplaintsRouter from "./routes/complaint.route";
@@ -14,21 +9,12 @@ import ComplaintsRouter from "./routes/complaint.route";
 const port = env.PORT;
 const app = express();
 
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(express.json()); 
+app.use(cookieParser());
 
-<<<<<<< HEAD
-app.use("/api/auth", router);
-
-// app.use(cookieParser());
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:5000");
-   connectDB();
-});
-=======
 app.use("/api/auth", router); // Use the auth routes
 app.use("/", ComplaintsRouter); // Use the Complaints routes
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on http://localhost:${port}`);
 });
->>>>>>> 0f56d3f9fd3c100be18820f24a4c0564a88d9d1d
