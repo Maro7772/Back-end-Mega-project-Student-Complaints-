cef9432ed91eaa4be27df4a28993fd70c853ab0f
Maro7772 <mm9922522@gmail.com>
2025-05-08 19:29:20 +0300

    FIXING URL OF FRONT_END (cors)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"; // Load environment variables from .env file
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors"; // Import CORS middleware
 import env from "./util/validateEnv";
 import { connectDB  } from "./config/db";
 import router from "./routes/auth.route";
@@ -8,6 +9,7 @@ import ComplaintsRouter from "./routes/complaint.route";
 const port = env.PORT;
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.use("/api/auth", router); // Use the auth routes
